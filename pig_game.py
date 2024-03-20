17c46d1b70665dadeff365f348cc330b47cc021a
glennjs <163595308+glennjs@users.noreply.github.com>
2024-03-19 23:12:59 -0400

    Update pig_game.py

--- pig_game.py
+++ pig_game.py
@@ -1,11 +1,12 @@
-import random
+#This code does not work for some reason. The game will still continue even if the score hits or passes 50.
 
+
+import random
 
 def roll():
     min_value = 1
     max_value = 6 
     roll = random.randint(min_value, max_value)
-
     return roll
 
 while True:
@@ -19,8 +20,34 @@ while True:
     else:
         print("Invalid, please try again.")
 
-
 max_score = 50
 player_scores = [0 for _ in range(players)]
 
-print(player_scores)
+while max(player_scores) < max_score:
+    for player_index in range(players):
+        print("\nPlayer", player_index + 1, "'s turn has just started!\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1: 
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+        
+        player_scores[player_index] += current_score
+        print("Your total score is:", player_scores[player_index])
+
+     
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player number", winning_index + 1, "is the winner with a score of:", max_score)
