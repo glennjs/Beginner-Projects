bb690d19e8ae7acf0890b8a8b6f2045846db085a
glennjs <163595308+glennjs@users.noreply.github.com>
2024-03-19 22:00:16 -0400

    Add files via upload

---
+++ pig_game.py
@@ -0,0 +1,26 @@
+import random
+
+
+def roll():
+    min_value = 1
+    max_value = 6 
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2-4 players.")
+    else:
+        print("Invalid, please try again.")
+
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+print(player_scores)
